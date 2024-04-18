709ade9cd98aed7830fb1f4a35eee36226707610
Alexander Dadiani <alexdadiani1994@gmail.com>
2024-04-18 22:32:10 +0200

    add start reblacktree

---
+++ exercises/redblacktree/rbt.go
@@ -0,0 +1,9 @@
+package redblacktree
+
+// Red-Black Binary Search Tree (BST) is a type of self balancing binary search tree.
+// A binary search tree consists of Nodes which have up to 2 children. The
+// children are ordered so that the value of a left child is lower than the
+// value of the right child. A BST can become unbalanced if the heights of the
+// left and right subtrees differ significantly leading to the BST degrading to
+// a linked list.
+// Multiple self balancing strategies exist to prevent such cases, e.g. Red-Black BSTs.
